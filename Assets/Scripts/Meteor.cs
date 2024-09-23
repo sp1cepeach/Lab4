933ed033c3af1420f7d97c1173cca3e050921544
sp1cepeach <alexkotzeva6@gmail.com>
2024-09-23 00:01:48 -0400

    Push

---
+++ Assets/Scripts/Meteor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * 2f);
+
+        if (transform.position.y < -11f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatIHit)
+    {
+        whatIHit.gameObject.hitByMeteor();
+        Destroy(this.gameObject);
+    }
+}
